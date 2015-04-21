e61f0300b45eddc6ce03e899fe086fe95175e7b5
Daniel Koh <daniel.wkoh@yahoo.com.sg>
2015-04-21 08:29:55 -0700

    Adding iOS Client

---
+++ ios/Phochat/Phochat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Phochat
+//
+//  Created by Daniel Koh on 4/13/15.
+//  Copyright (c) 2015 Carunaga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
