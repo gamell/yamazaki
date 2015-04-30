17db113840b94f529d3c23267a37a8bb60a993a6
Joan Gamell <gamell@gmail.com>
2015-04-30 00:53:56 -0700

    Getting the test images form parse
    yay!

--- visualizer/app/scripts/main.js
+++ visualizer/app/scripts/main.js
@@ -1,2 +1,50 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+/* jshint devel:true, browser: true*/
+/* globals Parse, jQuery */
+
+(function($){
+
+   'use strict';
+
+    var FINAL = Object.freeze({
+            slideshowSelector: '#slideshow'
+        });
+
+    var initParse = function initParse(){
+        Parse.initialize('9CqsDKHNK3zKxYKsOUik8n3gE4qusfIgU2NRDh5m', 'UR2IOAd8kxtPRfSoCEeGcmfxMLiXGl16acr4lg6S');
+    };
+
+    var generatePhotoElements = function generatePhotoElements(userPhotos){
+        var photoElements = [];
+        $.each(userPhotos, function(i, photo){
+            photoElements.push('<img src="'+photo.get('imageFile').url()+'" />');
+        });
+        return photoElements;
+    };
+
+    var refreshPhotos = function renderPhotos(userPhotos){
+        var photoElements = generatePhotoElements(userPhotos);
+        $(FINAL.slideshowSelector).empty().append(photoElements);
+    };
+
+    var getPictures = function getPictures(){  
+        var UserPhoto = Parse.Object.extend('UserPhoto');
+        var query = new Parse.Query(UserPhoto);
+        query.find({
+            success: function(results) {
+                refreshPhotos(results);
+            },
+            error: function(error) {
+                console.log('Error: ' + error.code + ' ' + error.message);
+            }
+        });
+    };
+
+    var init = function init(){
+        initParse();    
+        getPictures();     
+    };
+
+    init();
+
+})(jQuery);
+
